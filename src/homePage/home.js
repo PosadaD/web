cb268cabd8afcbb2d981b85919937d0abb56e6f9
posada <dapiposada@>
2023-08-17 23:59:13 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   src/homePage/components/loading.css 	new file:   src/homePage/components/particles.js 	modified:   src/homePage/home.css 	modified:   src/homePage/home.js 	new file:   src/homePage/img/Dogleft-size.png

--- src/homePage/home.js
+++ src/homePage/home.js
@@ -1,6 +1,11 @@
 import "./home.css"
 import Loading from "./components/loadig";
 import { useEffect, useRef, useState } from "react";
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+import dogLeft from "./img/Dogleft-size.png"
+import Particle from "./components/particles";
+
 
 
 function Home(){
@@ -21,7 +26,7 @@ function Home(){
             // img.src = image;
           });
         })
-      );  
+      );    
     };
 
     preloadImages()
@@ -38,7 +43,7 @@ function Home(){
 
     //tipyng text animation
     const [typetext, settypetext] = useState("")
-    var txt = "dieguillo el mas guapillo";
+    var txt = "  Hey there! I'm a 23-year-old student of Systems Engineering who's also diving headfirst into the world of tech as a front-end developer. You see, I've got this dream of becoming a full stack developer someday. My passion for technology started from a place of lending a hand to those struggling to grasp it – because, let's face it, we've all been there at some point. So, as I navigate the complexities of coding and design, I'm not just crafting digital experiences; I'm weaving my way through a journey that's as much about helping others as it is about my own growth.";
     var speed = 50;
 
     useEffect(() => {
@@ -57,6 +62,8 @@ function Home(){
 
 
 
+    //animation section 
+
 
     return(
         <>
@@ -67,6 +74,7 @@ function Home(){
             :
                 <div className="homeconatiner">
                     <div className="section main">
+                        <Particle />
                         <div className="mainsec mainsection1">
                             <div className="mainsection1Title">
                                 <div className="mainsection1TitleName">
@@ -90,7 +98,23 @@ function Home(){
                         section1
                     </div>
                     <div className="section contact">
-                        section1
+                        <div className="contactsec contactsection1">
+                            <div className="contactsection1Title">
+                                <h1>Let's make something</h1>
+                                <h1>&nbsp;awesome together.</h1>
+                            </div>
+                            <Fade bottom>
+                                <div className="contactsection1Text">
+                                    <h4>Feel free to write to me to work together, for any questions/comments or just to say hello.</h4>
+                                    <h1>dapiposada@gmail.com</h1>
+                                </div>
+                            </Fade>             
+                        </div>
+                        <div className="contactsec contactsection2">
+                            <Fade right>
+                                <img src={dogLeft}></img>
+                            </Fade>
+                        </div>
                     </div>   
                 </div>
             }
